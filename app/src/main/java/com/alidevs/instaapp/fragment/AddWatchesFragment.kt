2cd52e2ed53cca8b3809dfa04d8afd65aca720a9
aasif1297 <aasif1297@gmail.com>
2019-05-02 19:45:16 +0500

    multiple images upload feature on firebase storage

--- app/src/main/java/com/alidevs/instaapp/fragment/AddWatchesFragment.kt
+++ app/src/main/java/com/alidevs/instaapp/fragment/AddWatchesFragment.kt
@@ -15,18 +15,13 @@ import android.widget.Toast
 import com.alidevs.instaapp.R
 import com.alidevs.instaapp.utils.AppPreferences
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import com.theartofdev.edmodo.cropper.CropImage
-import id.zelory.compressor.Compressor
 import kotlinx.android.synthetic.main.fragment_add_watches.*
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.IOException
-import java.util.*
+
 
 class AddWatchesFragment : AppCompatActivity() {
 
@@ -38,7 +33,15 @@ class AddWatchesFragment : AppCompatActivity() {
     private lateinit var compressedImageFile: Bitmap
     private lateinit var user_id: String
     private lateinit var utils: AppPreferences
+    //private lateinit var imgList: List
 
+
+    var SELECT_PICTURES = 1;
+
+    private var mArrayUri = ArrayList<Uri>();
+    private var imageUri: Uri? = null;
+    private var up = 0;
+    private var k = 0;
 
     private lateinit var primaryImageURI: Uri
     private lateinit var firstImageURI: Uri
@@ -85,11 +88,147 @@ class AddWatchesFragment : AppCompatActivity() {
         }
 
         btn_save.setOnClickListener {
-            /*sendDataToStore(primaryImageURI)
-            sendDataToStore(firstImageURI)
-            sendDataToStore(secondImageURI)
-            sendDataToStore(thirdImageURI)*/
+            progressBar2.visibility = View.VISIBLE
+            var imgurl = ArrayList<String>()
+            val filepath = FirebaseStorage.getInstance().reference.child("watches_images")
+
+            while (up < mArrayUri.size) {
+                val ref = storageReference.child("watches_post_images").child("${mArrayUri[k].lastPathSegment}.jpg")
+                val filepath = ref.putFile(mArrayUri[k])
+                filepath.addOnSuccessListener { task ->
+                    ref.downloadUrl.addOnSuccessListener { downloadPhotoUrl ->
+                        //Now play with downloadPhotoUrl
+                        //Store data into Firebase Realtime Database
+                        val downloadURL = downloadPhotoUrl
+                        imgurl.add(downloadURL.toString())
+                        Toast.makeText(this, downloadURL.toString(), Toast.LENGTH_SHORT).show()
+                        Log.d("#IMAGEURL", downloadURL.toString())
+                        /* val friendlyMessage = FriendlyMessage(null, mUsername, downloadPhotoUrl.toString())
+                                        mDatabaseReference.push().setValue(friendlyMessage)*/
+                    }
+                }
+                up++
+                k++
+            }
+
+
+            if (edi_brand.text.toString() != "" && edt_model.text.toString() != "" && edt_ref.text.toString() != "" &&
+                edt_serial.text.toString() != "" && edt_date.text.toString() != "" && edt_comment.text.toString() != ""
+            ) {
+                val items = HashMap<String, Any>()
+                items["image_url_primary"] = imgurl[0]
+                items["image_url_first"] = imgurl[1]
+                items["image_url_second"] = imgurl[2]
+                items["image_url_third"] = imgurl[3]
+                items["brand_name"] = edi_brand.text.toString()
+                items["model"] = edt_model.text.toString()
+                items["reference"] = edt_ref.text.toString()
+                items["serial"] = edt_serial.text.toString()
+                items["purchase_date"] = edt_date.text.toString()
+                items["comments"] = edt_comment.text.toString()
+                //items["timestamp"] = FieldValue.serverTimestamp()
+
+                firestore.collection("MyWatches").document(user_id).set(items).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        progressBar2.visibility = View.GONE
+                        Toast.makeText(this, "Post was added", Toast.LENGTH_LONG).show()
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, "FireStore Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "All Fields are mandatory to fill", Toast.LENGTH_LONG).show()
+            }
+
+            /*  firestore.collection("MyWatches").add(items)
+                  .addOnCompleteListener { task ->
+                      if (task.isSuccessful) {
+                          progressBar2.visibility = View.GONE
+                          Toast.makeText(this, "Post was added", Toast.LENGTH_LONG).show()
+                      }
+                  }.addOnFailureListener {
+                      Toast.makeText(this, "FireStore Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                  }
+          } else {
+              Toast.makeText(this, "All Fields are mandatory to fill", Toast.LENGTH_LONG).show()
+          }*/
+
+
         }
+        /* val filepath = FirebaseStorage.getInstance().reference.child("gpic");
+
+         while (up < mArrayUri.size) {
+             val randonName = UUID.randomUUID().toString()
+             val primaryImgage = File(mArrayUri[k].path)
+             //Primary Image
+             try {
+                 compressedImageFile = Compressor(this)
+                     .setMaxHeight(720)
+                     .setMaxWidth(720)
+                     .setQuality(50)
+                     .compressToBitmap(primaryImgage)
+             } catch (e: IOException) {
+                 e.printStackTrace()
+             }
+             val byte = ByteArrayOutputStream()
+             compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, byte)
+             val imagedata = byte.toByteArray()
+             val ref = storageReference.child("watches_post_images").child("$randonName.jpg")
+             val filepath = ref.putBytes(imagedata)
+             filepath.addOnFailureListener {
+                 Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+             }.addOnSuccessListener {
+                 val downloadurl = ref.downloadUrl
+                 if (it.task.isSuccessful) {
+                     val newthumbfile = File(primaryImgage.path)
+                     try {
+                         compressedImageFile = Compressor(this)
+                             .setMaxHeight(100)
+                             .setMaxWidth(100)
+                             .setQuality(1)
+                             .compressToBitmap(newthumbfile)
+                     } catch (e: IOException) {
+                         e.printStackTrace()
+                     }
+                     val byte = ByteArrayOutputStream()
+                     compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, byte)
+                     val thumbdata = byte.toByteArray()
+                     val uploadTask: UploadTask = storageReference.child("watches_post_images/thumbs")
+                         .child("$.jpg").putBytes(thumbdata)
+
+
+                     uploadTask.addOnFailureListener {
+                         Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                     }.addOnSuccessListener {
+                         val downloadthumburl = downloadurl.result
+                         val items = HashMap<String, Any>()
+                         items["image_url_primary"] = downloadurl.result.toString()
+                         items["image_thumb_primary"] = downloadthumburl.toString()
+                         items["image_url_first"] = downloadurl.result.toString()
+                         items["image_thumb_first"] = downloadthumburl.toString()
+                         items["image_url_second"] = downloadurl.result.toString()
+                         items["image_thumb_second"] = downloadthumburl.toString()
+                         items["image_url_third"] = downloadurl.result.toString()
+                         items["image_thumb_third"] = downloadthumburl.toString()
+                         items["reference"] = downloadthumburl.toString()
+                         //items["timestamp"] = FieldValue.serverTimestamp()
+
+                         firestore.collection("Posts").add(items)
+                             .addOnCompleteListener { task ->
+                                 if (task.isSuccessful) {
+                                     Toast.makeText(this, "Post was added", Toast.LENGTH_LONG).show()
+                                 }
+                             }.addOnFailureListener {
+                                 Toast.makeText(this, "FireStore Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                             }
+                     }
+                 }
+                 up++
+                 k++
+
+             }
+         }*/
+
     }
 
     private fun selectImage() {
@@ -103,6 +242,22 @@ class AddWatchesFragment : AppCompatActivity() {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             val result = CropImage.getActivityResult(data)
             if (resultCode == Activity.RESULT_OK) {
+                /*if (data!!.clipData != null) {
+                    val count = data.clipData.itemCount
+                    Log.i("count", "$count")
+                    var currentItem = 0
+                    while (currentItem < count) {
+                        imageUri = data.clipData.getItemAt(currentItem).uri
+
+
+                        Log.i("uri", imageUri.toString())
+                        mArrayUri.add(imageUri!!)
+                        currentItem += 1
+                    }
+                    Log.i("listsize", "${mArrayUri.size}")
+                } else if (data.data != null) {
+                    var imagePath = data.data.path
+                }*/
                 //val resultUri = result.uri
                 if (primary) {
                     primaryImageURI = result.uri
@@ -111,6 +266,7 @@ class AddWatchesFragment : AppCompatActivity() {
                     val d = Drawable.createFromPath(f.absolutePath)
                     constraintLayout3.background = d
                     primary = false
+                    mArrayUri.add(primaryImageURI)
                 }
                 if (first) {
                     firstImageURI = result.uri
@@ -119,6 +275,7 @@ class AddWatchesFragment : AppCompatActivity() {
                     val d = Drawable.createFromPath(f.absolutePath)
                     first_img.background = d
                     first = false
+                    mArrayUri.add(firstImageURI)
                 }
                 if (second) {
                     secondImageURI = result.uri
@@ -127,6 +284,7 @@ class AddWatchesFragment : AppCompatActivity() {
                     val d = Drawable.createFromPath(f.absolutePath)
                     second_img.background = d
                     second = false
+                    mArrayUri.add(secondImageURI)
                 }
                 if (third) {
                     thirdImageURI = result.uri
@@ -135,6 +293,7 @@ class AddWatchesFragment : AppCompatActivity() {
                     val d = Drawable.createFromPath(f.absolutePath)
                     third_img.background = d
                     third = false
+                    mArrayUri.add(thirdImageURI)
                 }
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 val error = result.error
@@ -144,7 +303,7 @@ class AddWatchesFragment : AppCompatActivity() {
     }
 
     private fun getRealPathFromURI(contentURI: Uri): String? {
-        val cursor: Cursor? =  contentResolver.query(contentURI, null, null, null, null)
+        val cursor: Cursor? = contentResolver.query(contentURI, null, null, null, null)
         return if (cursor == null) { // Source is Dropbox or other similar local file path
             contentURI.path
         } else {
@@ -155,14 +314,37 @@ class AddWatchesFragment : AppCompatActivity() {
     }
 
 
-
     private fun sendDataToStore() {
-        val randonName = UUID.randomUUID().toString()
-        val primaryImgage = File(primaryImageURI.path)
-        /*val newimage = File(primaryImageURI.path)
-        val newimage = File(primaryImageURI.path)
-        val newimage = File(primaryImageURI.path)*/
 
+
+        /* var filepath = FirebaseStorage.getInstance().reference.child("gpic");
+
+        while (up < mArrayUri.size()){
+
+            filepath.child(mArrayUri.get(k).getLastPathSegment()).putFile(mArrayUri.get(k)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadURL = taskSnapshot.getDownloadUrl();
+                    Toast.makeText(TestingActivity.this, downloadURL.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            up++;
+            k++;
+
+        }*/
+
+    }
+
+
+    /*val randonName = UUID.randomUUID().toString()
+        val primaryImgage = File(primaryImageURI.path)
+        val firstImage = File(firstImageURI.path)
+        val secondImage = File(secondImageURI.path)
+        val ThirdImage = File(thirdImageURI.path)
+
+
+        //Primary Image
         try {
             compressedImageFile = Compressor(this)
                 .setMaxHeight(720)
@@ -172,7 +354,6 @@ class AddWatchesFragment : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
-
         val byte = ByteArrayOutputStream()
         compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, byte)
         val imagedata = byte.toByteArray()
@@ -196,61 +377,38 @@ class AddWatchesFragment : AppCompatActivity() {
                 val byte = ByteArrayOutputStream()
                 compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, byte)
                 val thumbdata = byte.toByteArray()
-                val ref = storageReference.child("watches_post_images").child("$randonName.jpg")
-                val filepath = ref.putBytes(imagedata)
-                filepath.addOnFailureListener {
+                val uploadTask: UploadTask = storageReference.child("watches_post_images/thumbs")
+                    .child("$randonName.jpg").putBytes(thumbdata)
+
+
+                uploadTask.addOnFailureListener {
                     Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
                 }.addOnSuccessListener {
-                    val downloadurl = ref.downloadUrl
-                    if (it.task.isSuccessful) {
-                        val newthumbfile = File(primaryImgage.path)
-                        try {
-                            compressedImageFile = Compressor(this)
-                                .setMaxHeight(100)
-                                .setMaxWidth(100)
-                                .setQuality(1)
-                                .compressToBitmap(newthumbfile)
-                        } catch (e: IOException) {
-                            e.printStackTrace()
+                    val downloadthumburl = downloadurl.result
+                    val items = HashMap<String, Any>()
+                    items["image_url_primary"] = downloadurl.result.toString()
+                    items["image_thumb_primary"] = downloadthumburl.toString()
+                    items["image_url_first"] = downloadurl.result.toString()
+                    items["image_thumb_first"] = downloadthumburl.toString()
+                    items["image_url_second"] = downloadurl.result.toString()
+                    items["image_thumb_second"] = downloadthumburl.toString()
+                    items["image_url_third"] = downloadurl.result.toString()
+                    items["image_thumb_third"] = downloadthumburl.toString()
+                    items["reference"] = downloadthumburl.toString()
+                    items["timestamp"] = FieldValue.serverTimestamp()
+
+                    firestore.collection("Posts").add(items)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "Post was added", Toast.LENGTH_LONG).show()
+                            }
+                        }.addOnFailureListener {
+                            Toast.makeText(this, "FireStore Error: ${it.message}", Toast.LENGTH_SHORT).show()
                         }
-                        val byte = ByteArrayOutputStream()
-                        compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, byte)
-                        val thumbdata = byte.toByteArray()
-                        val uploadTask: UploadTask = storageReference.child("watches_post_images/thumbs")
-                            .child("$randonName.jpg").putBytes(thumbdata)
-
-
-
-
-
-                        uploadTask.addOnFailureListener {
-                            Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
-                        }.addOnSuccessListener {
-                            val downloadthumburl = downloadurl.result
-                            val items = HashMap<String, Any>()
-                            items["image_url_primary"] = downloadurl.result.toString()
-                            items["image_thumb_primary"] = downloadthumburl.toString()
-                            items["image_url_first"] = downloadurl.result.toString()
-                            items["image_thumb_first"] = downloadthumburl.toString()
-                            items["image_url_second"] = downloadurl.result.toString()
-                            items["image_thumb_second"] = downloadthumburl.toString()
-                            items["image_url_third"] = downloadurl.result.toString()
-                            items["image_thumb_third"] = downloadthumburl.toString()
-                            items["reference"] = downloadthumburl.toString()
-                            items["timestamp"] = FieldValue.serverTimestamp()
-
-                            firestore.collection("Posts").add(items)
-                                .addOnCompleteListener { task ->
-                                    if (task.isSuccessful) {
-                                        Toast.makeText(this, "Post was added", Toast.LENGTH_LONG).show()
-                                    }
-                                }.addOnFailureListener {
-                                    Toast.makeText(this, "FireStore Error: ${it.message}", Toast.LENGTH_SHORT).show()
-                                }
-                        }
-                    }
                 }
             }
         }
-    }
+}*/
 }
+
+
